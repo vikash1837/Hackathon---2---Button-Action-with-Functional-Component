fdc0b5c63a8a081dd239310d57b3fc5d4d52274d
Vikash kumar <64901451+vikash1837@users.noreply.github.com>
2020-10-25 23:27:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const[parag,setParag]=useState();
+  const[para,setPara]=useState();
   function Addpara(){
     setPara(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>);
   }
   return (
     <div id="main">
-       <p>{parag}</p>
+       <p>{para}</p>
       <button id="click" onClick={Addpara}>button</button>
       // Do not alter the main div
     </div>
