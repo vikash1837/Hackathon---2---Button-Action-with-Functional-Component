d5972910b46601421941700a63d439e5053475ee
Vikash kumar <64901451+vikash1837@users.noreply.github.com>
2020-10-25 22:53:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import "./../styles/App.css";
 function App() {
   const[para,setPara]=useState("");
   function Addpara(){
-    setPara(<p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>);
+    setPara(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>);
   }
   return (
     <div id="main">
-       <p id="para">{para}</p>
+       <p>{para}</p>
       <button id="click" onClick={Addpara}>button</button>
       // Do not alter the main div
     </div>
